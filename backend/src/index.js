5c9c69aa8671db70cab64416c0456fb0455a03b0
Zeiad Magdy <zeiadmagdy2019@gmail.com>
2025-02-11 20:04:38 +0200

    added packages

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 
 
@@ -14,6 +15,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
